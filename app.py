c9299cdba4551a93625007dfd1e3a954f4e0448b
Silent YANG <yang@vicicode.com>
2024-02-04 22:30:25 +0800

    update

---
+++ app.py
@@ -0,0 +1,63 @@
+import head
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify
+from flask_cors import cross_origin
+import ephem
+
+app = Flask(__name__)
+
+
+def local2utc(local_dtm):
+    # 本地时间转 UTC 时间（ -8:00 ）
+    return datetime.utcfromtimestamp(local_dtm.timestamp())
+
+
+@app.route("/pass", methods=['POST'])
+@cross_origin()
+def satpass():
+    sat = request.json.get('sat', '')
+    sat_line_1 = request.json.get('sat_line_1', '')
+    sat_line_2 = request.json.get('sat_line_2', '')
+    lat = request.json.get('lat', 0)
+    lng = request.json.get('lng', 0)
+    alt = request.json.get('alt', 0)
+
+    target_satellite, find_flag = head.FIND_SATE(sat_line_1, sat_line_2, sat)
+    pass_times, departure_times = head.CAL_PASS_TIME(target_satellite,
+                                                     float(lat), float(lng),
+                                                     float(alt))
+
+    return jsonify({
+        'code': 200,
+        'pass_times': pass_times,
+        'departure_times': departure_times
+    })
+
+
+@app.route("/doppler", methods=['POST'])
+@cross_origin()
+def doppler():
+    sat = request.json.get('sat', '')
+    sat_line_1 = request.json.get('sat_line_1', '')
+    sat_line_2 = request.json.get('sat_line_2', '')
+    lat = request.json.get('lat', 0)
+    lng = request.json.get('lng', 0)
+    alt = request.json.get('alt', 0)
+    tx = request.json.get('tx', 0)
+    rx = request.json.get('rx', 0)
+    format = "%Y-%m-%d %H:%M:%S"
+    pass_time = datetime.strptime(request.json.get('pass_time', ''), format)
+    departure_time = datetime.strptime(request.json.get('departure_time', ''),
+                                       format)
+    satellite = ephem.readtle(sat, sat_line_1, sat_line_2)
+    print(pass_time + " " + local2utc(pass_time))
+    shift_array = []
+    while pass_time < departure_time:
+        AZ, EI, SHITF_UP, SHIFT_DOWN, DIS = head.CAL_DATA(
+            satellite, sat_line_1, sat_line_2, float(lng), float(lat),
+            float(alt), local2utc(pass_time),
+            float(tx) * 1000000,
+            float(rx) * 1000000)
+        shift_array.append([SHITF_UP, SHIFT_DOWN])
+        pass_time = pass_time + timedelta(seconds=2)
+    return jsonify({'code': 200, 'shift_array': shift_array})
---
+++ head.py
@@ -0,0 +1,74 @@
+from skyfield.api import Topos, load, utc, EarthSatellite
+from datetime import timedelta
+import datetime
+import math
+import pytz
+import ephem
+
+def FIND_SATE(line1, line2, satellite_name):
+    ts = load.timescale()
+    line1 = line1
+    line2 = line2
+    satellite = EarthSatellite(line1, line2, satellite_name, ts)
+    return satellite, 1
+
+
+def find_lines_after_string(filename, search_string):
+    line1 = None
+    line2 = None
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for i, line in enumerate(lines):
+            if search_string in line:
+                if i+1 < len(lines):
+                    line1 = lines[i+1].strip()
+                if i+2 < len(lines):
+                    line2 = lines[i+2].strip()
+                break
+    return line1, line2
+def CAL_PASS_TIME(target_satellite, observer_lat, observer_lon, observer_elevation):
+    topos = Topos(latitude_degrees=observer_lat, longitude_degrees=observer_lon,
+                  elevation_m=observer_elevation)
+    # 获取当前时间并创建Skyfield的时间对象
+    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+    ts = load.timescale()
+    t0 = ts.utc(now)
+    t1 = t0 + timedelta(days=2)  # 查找2天内过境时间
+    passes = target_satellite.find_events(topos, t0, t1)
+    # 获取北京时区
+    beijing_tz = pytz.timezone('Asia/Shanghai')
+
+    pass_times = []
+    departure_times = []
+    if not passes[0]:
+        return None, None
+    # 打印下一次过境的北京时间和离境时间
+    for time, event in zip(passes[0], passes[1]):
+        if event == 0:  # 0表示升，1表示降
+            # 将UTC时间转换为北京时间
+            beijing_time = time.utc_datetime().replace(tzinfo=pytz.utc).astimezone(beijing_tz)
+            pass_times.append(beijing_time.strftime('%Y-%m-%d %H:%M:%S'))
+        elif event == 2:
+            departure_time_utc = time.utc_datetime().replace(tzinfo=pytz.utc).astimezone(beijing_tz)
+            departure_times.append(departure_time_utc.strftime('%Y-%m-%d %H:%M:%S'))
+
+    return pass_times, departure_times
+
+def CAL_DATA(satellite,line1,line2,observer_lon,observer_lat,observer_elevation,data,UP_HZ,DOWN_HZ):
+
+
+    # 创建观测者对象并设置位置和时间
+    observer = ephem.Observer()
+    observer.lon = str(observer_lon)
+    observer.lat =str( observer_lat)
+    observer.elevation = observer_elevation
+    observer.date = data  # 设置为当前UTC时间
+
+    satellite.compute(observer)
+    # 获取卫星的速度（单位：km/s）
+    range_rate = satellite.range_velocity / 1000
+    # print("速度:", range_rate, "km/s")
+    UP_SHIFT = -range_rate / 299792.458 * UP_HZ  # 单位：Hz
+    DOWN_SHIFT = -range_rate / 299792.458 * DOWN_HZ  # 单位：Hz
+    DIS = satellite.range / 1000  # 单位从m转换为km
+    return round(math.degrees(float(satellite.az)),2),   round(math.degrees(float(satellite.alt)),2),round(UP_SHIFT,0),round(DOWN_SHIFT,0),round(DIS,2)
